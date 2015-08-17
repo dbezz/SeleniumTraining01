9dca3ffee1278f17076105b487a03844773ad8d6
dbezz <dmitrybezzubikov@gmail.com>
2015-08-17 19:00:03 +0300

    Rejected commit: Default

---
+++ src/main/java/Entities/AirExpense.java
@@ -0,0 +1,166 @@
+package Entities;
+
+/**
+ * Created by dmytro.bezzubikov on 8/17/2015.
+ */
+public class AirExpense
+{
+    private String personalAmount;
+    private String businessAmount;
+    private String amount;
+    private String transactionDate;
+    private String travelDate;
+    private String merchant;
+    private String ticketNumber;
+    private String booking;
+    private String carrier;
+    private String flightNo;
+    private String fromLocation;
+    private String toLocation;
+    private String purpose;
+    private String carrierExp;
+    private String fromLocationExp;
+    private String toLocationExp;
+
+    public String getOutReason() {
+        return outReason;
+    }
+
+    public void setOutReason(String outReason) {
+        this.outReason = outReason;
+    }
+
+    private String outReason;
+
+    public String getPersonalAmount() {
+        return personalAmount;
+    }
+
+    public void setPersonalAmount(String personalAmount) {
+        this.personalAmount = personalAmount;
+    }
+
+    public String getBusinessAmount() {
+        return businessAmount;
+    }
+
+    public void setBusinessAmount(String businessAmount) {
+        this.businessAmount = businessAmount;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+
+    public String getTransactionDate() {
+        return transactionDate;
+    }
+
+    public void setTransactionDate(String transactionDate) {
+        this.transactionDate = transactionDate;
+    }
+
+    public String getTravelDate() {
+        return travelDate;
+    }
+
+    public void setTravelDate(String travelDate) {
+        this.travelDate = travelDate;
+    }
+
+    public String getMerchant() {
+        return merchant;
+    }
+
+    public void setMerchant(String merchant) {
+        this.merchant = merchant;
+    }
+
+    public String getTicketNumber() {
+        return ticketNumber;
+    }
+
+    public void setTicketNumber(String ticketNumber) {
+        this.ticketNumber = ticketNumber;
+    }
+
+    public String getBooking() {
+        return booking;
+    }
+
+    public void setBooking(String booking) {
+        this.booking = booking;
+    }
+
+    public String getCarrier() {
+        return carrier;
+    }
+
+    public void setCarrier(String carrier) {
+        this.carrier = carrier;
+    }
+
+    public String getFlightNo() {
+        return flightNo;
+    }
+
+    public void setFlightNo(String flightNo) {
+        this.flightNo = flightNo;
+    }
+
+    public String getFromLocation() {
+        return fromLocation;
+    }
+
+    public void setFromLocation(String fromLocation) {
+        this.fromLocation = fromLocation;
+    }
+
+    public String getToLocation() {
+        return toLocation;
+    }
+
+    public void setToLocation(String toLocation) {
+        this.toLocation = toLocation;
+    }
+
+    public String getPurpose() {
+        return purpose;
+    }
+
+    public void setPurpose(String purpose) {
+        this.purpose = purpose;
+    }
+
+    public String getCarrierExp() {
+        return carrierExp;
+    }
+
+    public void setCarrierExp(String carrierExp) {
+        this.carrierExp = carrierExp;
+    }
+
+    public String getToLocationExp() {
+        return toLocationExp;
+    }
+
+    public void setToLocationExp(String toLocationExp) {
+        this.toLocationExp = toLocationExp;
+    }
+
+
+
+    public String getFromLocationExp() {
+        return fromLocationExp;
+    }
+
+    public void setFromLocationExp(String fromLocationExp) {
+        this.fromLocationExp = fromLocationExp;
+    }
+
+
+}
